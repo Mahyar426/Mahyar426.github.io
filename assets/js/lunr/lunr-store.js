ab4d01d7334f290415466b9143093a3140020f50
Mahyar426 <Mahyar426@users.noreply.github.com>
2025-06-30 16:10:25 +0000

    deploy: c78613814e2de8e4ed1edda1abc127faea50c633

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,403 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/blog/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "/blog/post-quote/",
+        "teaser": null
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "tags": ["link","Post Formats"],
+        "url": "/blog/post-link/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/comgarello1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/comgarello2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/comgarello3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/comgarello4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/crypto6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/dsp1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/dsp2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/digitalcomlab1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/digitalcomlab2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fpga6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/ivpl1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/ivpl2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labdsp1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labdsp3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labdsp4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labdsp5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labdsp2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/labfpga6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/linearalg/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mlproject/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/mm6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma7/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spma8/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/spwt/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat1/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat2/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat3/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat4/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat5/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sat6/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/satcomm/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/thesisb/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Details will be added later.  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/thesism/",
+        "teaser": null
+      },{
+        "title": "Sampling and Chirp Signals",
+        "excerpt":"Here, I’ll detail the sampling and chirp signal project with theory, algorithms, and results.      We generated chirp signals using a MATLAB script and analyzed their frequency response…  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/sampleproj/",
+        "teaser": null
+      }]
